8b6920e38cde80c261f2c39e4cbff1f1b2eaac5c
Ian Chan <icha024@gmail.com>
2016-02-15 23:31:43 +0000

    Initial commit

---
+++ src/main/java/com/clianz/web/Pet.java
@@ -0,0 +1,18 @@
+package com.clianz.web;
+
+public class Pet {
+
+	public Pet(String name) {
+		this.name = name;
+	}
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
